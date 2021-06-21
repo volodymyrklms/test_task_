516597ca050b03cc122d3e4d092dd219e69b96bf
Volodymyr <volodymyrklms@gmail.com>
2021-06-21 14:26:40 +0300

    test task updated

--- test_task_frontend/src/pages/treeSelect.tsx
+++ test_task_frontend/src/pages/treeSelect.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, ChangeEvent} from "react";
 import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
 import {FormControl, InputLabel, MenuItem, Select, Typography} from "@material-ui/core";
-import getNodes from "../services/treeService";
+import {getNodes, convertLabels} from "../services/treeService";
 import {TreeSelectInterface} from "../interfaces/treeSelect";
 
 
@@ -31,61 +31,54 @@ const useStyles = makeStyles((theme: Theme) =>
 
 
 const TreeSelect = () => {
-    const [selectsData, setSelectsData] = useState<Array<number>>([]);
+    const [selectsData, setSelectsData] = useState<number[]>([]);
     const [curIndex, setCurIndex] = useState<number>(0);
-    const [data, setData] = useState<Array<Array<TreeSelectInterface>>>([]);
+    const [data, setData] = useState<TreeSelectInterface[][]>([]);
     const [description, setDescription] = useState<string>('');
 
     const classes = useStyles();
 
     useEffect(() => {
-        if (data.length === 0) {
-            getNodes()
-                .then(res => {
-                    setData([res as Array<TreeSelectInterface>]);
-                });
-        }
-    })
-
-    useEffect(() => {
         getNodes(selectsData[curIndex])
             .then(res => {
-                res = res as Array<TreeSelectInterface>;
+                res = res as TreeSelectInterface[];
                 if (res[0].name === "DESCRIPTION") {
                     setDescription(res[0].text);
                 } else {
-                    const newData = [...data];
-                    newData.push(res);
-                    setData(newData);
+                    setData(prevState => [...prevState, convertLabels(res as TreeSelectInterface[])]);
                 }
             });
-    }, [selectsData]);
+    }, [selectsData, curIndex]);
 
     const selectChange = (e: ChangeEvent<{ name?: string | undefined; value: unknown; }>, index: number) => {
-        const newData: Array<number> = [...selectsData];
+        const newData: number[] = [...selectsData];
         newData[index] = e.target.value as number;
         setSelectsData(newData.slice(0, index + 1));
         setData(data.slice(0, index + 1));
         setCurIndex(index);
+        setDescription("");
     }
 
     return (
         <div className={classes.root}>
             <div className={classes.container}>
                 <FormControl>
-                    {description ? <Typography className={classes.text}>
-                        {description}
-                    </Typography> : null}
+                    {description ?
+                        <>
+                            <Typography className={classes.text}>
+                                <strong>Description:</strong> {description}
+                            </Typography>
+                        </> : null}
                 </FormControl>
                 {data.length > 0 ? data.map((select, index) => (
-                    <FormControl>
+                    <FormControl key={index}>
                         <InputLabel id={`${index}`}>{select[0].name}</InputLabel>
                         <Select
                             key={`${index}`}
                             className={classes.select}
                             id={`${index}`}
                             multiple={false}
-                            value={selectsData[index]}
+                            value={selectsData[index] ? selectsData[index] : ""}
                             onChange={(e) => selectChange(e, index)}
                         >
                             {select.map((option) => (
--- test_task_frontend/src/services/treeService.ts
+++ test_task_frontend/src/services/treeService.ts
@@ -1,20 +1,32 @@
 import axios from "axios";
 import {apiDomain} from "../constants";
 import {TreeSelectInterface} from "../interfaces/treeSelect";
+import labelsMapping from "../labels.json";
 
 function getNodes(parent: number | null = null) {
-    console.log(parent);
     const url = parent ? `${apiDomain}/data/?parent=${parent}` : `${apiDomain}/data/`;
     return axios.get(url)
         .then(res => {
             if (res.status !== 200) {
                 return Promise.reject(res);
             }
-            return Promise.resolve<Array<TreeSelectInterface>>(res.data);
+            return Promise.resolve<TreeSelectInterface[]>(res.data);
         })
         .catch(err => {
             console.log(err);
         });
 }
 
-export default getNodes;
+function convertLabels(data: Array<TreeSelectInterface>) {
+    function capitalizeString(data: string) {
+        return data.charAt(0).toUpperCase() + data.slice(1).toLowerCase();
+    }
+
+    data.forEach(select => {
+        select.name = Object.keys(labelsMapping).includes(select.name) ? Object(labelsMapping)[select.name] :
+            capitalizeString(select.name).replace("_", " ");
+    });
+    return data
+}
+
+export {getNodes, convertLabels};
